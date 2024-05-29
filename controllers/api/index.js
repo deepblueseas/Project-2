63c07c64eb89230b161030125df3f4f921bd1d18
dom <dominic.decapite@gmail.com>
2024-05-29 19:28:47 -0400

    Update both index.js for routes

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const userRoutes = require('./userRoutes');
+const projectRoutes = require('./storyRoutes');
+
+router.use('/users', userRoutes);
+router.use('/stories', storyRoutes);
+
+module.exports = router;
