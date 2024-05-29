5fa08a77ecc9560b992585e00ef5c398df0a64d1
JohnTD796 <156860528+JohnTD796@users.noreply.github.com>
2024-05-29 17:35:01 -0400

    changed structure a bit

--- seeds/index.js
+++ seeds/index.js
@@ -3,26 +3,37 @@ const seedPrompt = require('./promptData');
 const seedSegment = require('./segmentData');
 const seedStory = require('./storyData');
 const seedUser = require('./userData');
+const seedStorySegment = require('./storySegmentData');
 
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
+  
+  console.log('\n----- GENRES -----\n');
   await seedGenre();
   console.log('\n----- GENRES SEEDED -----\n');
 
+  console.log('\n----- PROMPTS -----\n');
   await seedPrompt();
   console.log('\n----- PROMPTS SEEDED -----\n');
 
+  console.log('\n----- USERS -----\n');
   await seedUser();
   console.log('\n----- USERS SEEDED -----\n');
 
-  await seedSegment();
-  console.log('\n----- SEGMENTS TAGS SEEDED -----\n');
-
+  console.log('\n----- STORIES -----\n');
   await seedStory();
   console.log('\n----- STORIES SEEDED -----\n');
+  
+  console.log('\n----- SEGMENTS -----\n');
+  await seedSegment();
+  console.log('\n----- SEGMENTS SEEDED -----\n');
+
+  console.log('\n----- STORY SEGMENTS -----\n');
+  await seedStorySegment();
+  console.log('\n----- STORY SEGMENTS SEEDED -----\n');
 
   process.exit(0);
 };
--- seeds/userData.js
+++ seeds/userData.js
@@ -1,30 +1,35 @@
-const User = require("../models/User");
+const User = require("../models/user");
 
 const userData = [
     {
         username: "user1",
         user_bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        email: "user1@email.com"
+        email: "user1@email.com",
+        password: "password1"
     },
     {
         username: "user2",
         user_bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        email: "user2@email.com"
+        email: "user2@email.com",
+        password: "password2"
     },
     {
         username: "user3",
         user_bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        email: "user3@email.com"
+        email: "user3@email.com",
+        password: "password3"
     },
     {
         username: "user4",
         user_bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        email: "user4@email.com"
+        email: "user4@email.com",
+        password: "password4"
     },
     {
         username: "user5",
         user_bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        email: "user5@email.com"
+        email: "user5@email.com",
+        password: "password5"
     },
     
 ]
