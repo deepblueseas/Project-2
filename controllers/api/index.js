aaa658c6f2acbe64fb39ed0ac5b5a91d3cc93652
Tess McGovern <ren.mcgovern@gmail.com>
2024-05-29 20:22:51 -0400

    server things done to get up and running

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./storyRoutes');
+// const userRoutes = require('./userRoutes');
+// const projectRoutes = require('./storyRoutes');
 
-router.use('/users', userRoutes);
-router.use('/stories', storyRoutes);
+// router.use('/users', userRoutes);
+// router.use('/stories', storyRoutes);
 
 module.exports = router;
