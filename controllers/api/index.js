4de0bc7fb97c64ddbe2a3933e279030409201a70
JohnTD796 <156860528+JohnTD796@users.noreply.github.com>
2024-05-30 21:17:16 -0400

    login/homepage route loads with story data

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
-// const projectRoutes = require('./storyRoutes');
+// const storyRoutes = require('./storyRoutes');
 
 router.use('/users', userRoutes);
 // router.use('/stories', storyRoutes);
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,23 +1,47 @@
 const express = require('express');
 const router = require('express').Router();
-const { User } = require('../models');
+const { Genre, Prompt, Segment, Story, User } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get('/homepage', withAuth, async (req, res) => {
+  try {
+      const storyData = await Story.findAll({
+          include: [
+              {
+                  model: Segment
+              },
+          ],
+      });
+
+      const stories = storyData.map((story) =>
+      story.get({ plain: true})
+  );
+console.log(stories[0].segments)
+  res.render('homepage', {
+      stories,
+      loggedIn: req.session.loggedIn,
+  });
+  } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+  }
+});
 
 router.get('/', withAuth, async (req, res) => {
     try {
         const userData = await User.findAll({
             attributes: { exclude: ['password'] },
-            order: [['name', 'ASC']],
+            order: [['username', 'ASC']],
         });
 
         const users = userData.map((project) => project.get({ plain: true }));
-
+console.log(req.session)
         res.render('homepage', {
             users,
-            // Pass the logged in flag to the template
-            logged_in: req.session.logged_in,
+            loggedIn: req.session.loggedIn,
           });
     } catch (err) {
+      console.log(err)
         res.status(500).json(err);
       }
 
@@ -25,8 +49,8 @@ router.get('/', withAuth, async (req, res) => {
 
 router.get('/login', (req, res) => {
     // If a session exists, redirect the request to the homepage
-    if (req.session.logged_in) {
-      res.redirect('/');
+    if (req.session.loggedIn) {
+      res.redirect('/homepage');
       return;
     }
   
