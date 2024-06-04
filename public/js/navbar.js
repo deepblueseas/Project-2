45579779ac63b88337873c7436c9a85391f4040c
Garrett <garrettbritton34@gmail.com>
2024-06-04 18:55:41 -0400

    "adding some css and README"

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,3 +1,14 @@
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-}
+// function toggleDarkMode() {
+//     document.body.classList.toggle('dark-mode');
+//     const isDarkMode = document.body.classList.contains('dark-mode');
+//     localStorage.setItem('mode', isDarkMode ? 'dark' : 'light');
+// }
+
+
+// document.getElementById('toggleLight').addEventListener('click', toggleDarkMode);
+
+
+// const mode = localStorage.getItem('mode');
+// if (mode === 'dark') {
+//     document.body.classList.add('dark-mode');
+// }
