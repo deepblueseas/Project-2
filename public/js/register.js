eb6c910a30c96afbd65a94a0b9a13b7ff600f7fd
Tess McGovern <ren.mcgovern@gmail.com>
2024-06-03 20:21:41 -0400

    we did it, new user can be registereeeddddgit add .

--- public/js/register.js
+++ public/js/register.js
@@ -1,34 +1,43 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const registerUserForm = document.getElementById('registerForm');
-    const createAccount = document.getElementById('createAccountButton');
+    const createAccount = document.getElementById('create-account-btn');
 
-    // createAccount.addEventListener('click', (event) => {
-    //     document.location.replace('/registerUsers');
+    if (registerUserForm && createAccount) {
+        createAccount.addEventListener('click', async (event) => {
+            console.log('click')
+            event.preventDefault();
 
-        const username = document.getElementById('inputNewUsername').value.trim();
-        const email = document.getElementById('inputNewEmail').value.trim();
-        const password = document.getElementById('inputNewPassword').value.trim();
+            const username = document.getElementById('inputNewUsername').value.trim();
+            const email = document.getElementById('inputNewEmail').value.trim();
+            const password = document.getElementById('inputNewPassword').value.trim();
 
-        try {
-            const response = fetch('/api/registerUsers/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ 
-                    username, 
-                    email,
-                    password,
-                })
-            });
+            try {
+                const response = await fetch('/registerUser/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ 
+                        username, 
+                        email,
+                        password,
+                    })
 
-            if (response.ok) {
-                document.location.replace('/homepage');
-            } else {
-                alert('Failed to create new account');
+                    
+                });
+
+                if (response.ok) {
+                    console.log('you did it')
+                    document.location.replace ('/homepage');
+                }
+
+            } catch (error) {
+                console.error('Error creating new account', error);
+                alert('An error occurred while creating your new account')
             }
-        } catch (error) {
-            console.error('Error creating new account', error);
-            alert('An error occurred while creating your new account')
-        }
-    });
+        });
+    } else {
+        console.error("Element with ID 'createAccountButton' not found.");
+    }
+});
+
